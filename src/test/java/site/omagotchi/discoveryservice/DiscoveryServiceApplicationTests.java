fcc7e04ecfb1d13174a5042459633f4a37e596fc
Quasimorphism <bak575755@gmail.com>
2026-08-18 17:39:41 +0900

    ci: dev 품질 분석 및 커버리지 기준 적용

--- src/test/java/site/omagotchi/discoveryservice/DiscoveryServiceApplicationTests.java
+++ src/test/java/site/omagotchi/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -3,9 +3,11 @@ package site.omagotchi.discoveryservice;
 import com.netflix.eureka.EurekaServerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class DiscoveryServiceApplicationTests {
@@ -20,6 +22,19 @@ class DiscoveryServiceApplicationTests {
     @Test
     void disablesReadOnlyResponseCache() {
         assertThat(this.eurekaServerConfig.shouldUseReadOnlyResponseCache()).isFalse();
+    }
+
+    @Test
+    void delegatesStartupToSpringApplication() {
+        String[] args = {"--server.port=0"};
+
+        try (var springApplication = mockStatic(SpringApplication.class)) {
+            DiscoveryServiceApplication.main(args);
+
+            springApplication.verify(
+                    () -> SpringApplication.run(DiscoveryServiceApplication.class, args)
+            );
+        }
     }
 
 }
